12d4f9b48b498dc316953e786a8d1efff3b1291b
Michael Le <mle.general@gmail.com>
2016-08-16 22:17:33 +0100

    Create base webpack with reload

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const validate = require('webpack-validator');
+const webpack = require('webpack');
+const merge = require('webpack-merge');
+
+const PATHS = {
+  app: path.join(__dirname, 'app'),
+  build: path.join(__dirname, 'build')
+};
+
+const common = {
+  // Entry accepts a path or an object of entries.
+  // We'll be using the latter form given it's
+  // convenient with more complex configurations.
+  entry: {
+    app: PATHS.app
+  },
+  output: {
+    path: PATHS.build,
+    filename: '[name].js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo'
+    })
+  ]
+}
+
+const parts = require('./libs/parts');
+
+var config;
+
+// Detect how npm is run and branch based on that
+switch(process.env.npm_lifecycle_event) {
+  case 'build':
+    config = merge(common, {});
+    break;
+  default:
+    config = merge(
+        common, 
+        parts.devServer({
+            // Customize host/port here if needed
+            host: process.env.HOST,
+            port: process.env.PORT
+        })
+    );
+}
+
+module.exports = validate(config);
